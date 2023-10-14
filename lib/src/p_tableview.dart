299c759d8d56698a602d301ffdd3fe9fc550c5ce
PandasCodi <110232127+PandasCodi@users.noreply.github.com>
2023-10-14 17:25:45 +0300

    pagination update, scrolling content fixed

--- lib/src/p_tableview.dart
+++ lib/src/p_tableview.dart
@@ -3,39 +3,68 @@ import 'package:pandas_tableview/src/p_tableview_content.dart';
 import 'package:pandas_tableview/src/p_tableview_header.dart';
 import 'package:pandas_tableview/src/p_tableview_pagination.dart';
 
-class PTableView extends StatelessWidget {
+class PTableView extends StatefulWidget {
   final PTableViewHeader header;
   final PTableViewContent content;
   final PTableViewPagination pagination;
+  final Color? paginationBackgroundColor;
   final double headerHeight;
   final BorderRadius? borderRadius;
   final double? fixedHeight;
-  const PTableView({Key? key, required this.header, required this.content, required this.pagination, required this.headerHeight, this.borderRadius, this.fixedHeight}) : super(key: key);
 
+  const PTableView(
+      {Key? key, required this.header, required this.content, this.paginationBackgroundColor, required this.pagination, required this.headerHeight, this.borderRadius, this.fixedHeight})
+      : super(key: key);
+
+  @override
+  State<PTableView> createState() => _PTableViewState();
+}
+
+class _PTableViewState extends State<PTableView> {
+  final ScrollController _scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: borderRadius ?? BorderRadius.zero,
+      borderRadius: widget.borderRadius ?? BorderRadius.zero,
       child: SizedBox(
-        height: fixedHeight,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(
-                  height: headerHeight,
-                  child: header),
-              fixedHeight != null ? Expanded(child: content) : content,
-              pagination != null ? Container(
+        height: widget.fixedHeight,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(child: Scrollbar(
+                controller: _scrollController,
+                child: SingleChildScrollView(
+                  controller: _scrollController,
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                          height: widget.headerHeight,
+                          child: widget.header),
+                      widget.fixedHeight != null ? Expanded(child: widget.content) : widget.content,
+                    ],
+                  ),
+                )
+            ),),
+            widget.pagination != null ? Container(
+              color: widget.paginationBackgroundColor ?? Colors.white,
+              child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: pagination,
-              ) : SizedBox.shrink()
-            ],
-          ),
-        )
+                child: widget.pagination,
+              ),
+            ) : SizedBox.shrink()
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
--- lib/src/p_tableview_content.dart
+++ lib/src/p_tableview_content.dart
@@ -8,12 +8,12 @@ class PTableViewContent extends StatefulWidget {
   final Color? backgroundColor;
   final List<PTableViewColumn> columns;
   final PTableViewPagination? pagination;
-  final EdgeInsets? contentPadding;
+  final double? horizontalPadding;
   final Decoration? decoration;
   final Function(int)? onTap;
 
   const PTableViewContent(
-      {Key? key, this.divider, required this.columns, this.onTap, this.backgroundColor, this.decoration, this.contentPadding, this.pagination})
+      {Key? key, this.divider, required this.columns, this.onTap, this.backgroundColor, this.decoration, this.horizontalPadding, this.pagination})
       : super(key: key);
 
   @override
@@ -26,51 +26,50 @@ class _PTableViewContentState extends State<PTableViewContent> {
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicWidth(
-        child: Container(
-          height: 200,
-            color: widget.backgroundColor,
-            decoration: widget.decoration,
-            child: RawScrollbar(
-              controller: _scrollController,
-              child: SingleChildScrollView(
-                controller: _scrollController,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [...widget.columns
-                      .asMap()
-                      .entries
-                      .map((e) =>
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          GestureDetector(onTap: () => widget.onTap?.call(e.key),
-                              child: MouseRegion(
-                                cursor: SystemMouseCursors.click,
-                                onEnter: (_) {
-                                  setState(() {
-                                    _highlightIndex = e.key;
-                                  });
-                                },
-                                onExit: (_) {
-                                  setState(() {
-                                    _highlightIndex = -1;
-                                  });
-                                },
-                                child: Container(
-                                  color: _highlightIndex == e.key ? Colors.grey
-                                      .withOpacity(0.2) : Colors.transparent,
-                                  child: Container(
-                                    padding: widget.contentPadding ?? EdgeInsets.zero,
-                                    child: e.value,
-                                  ),
-                                ),
-                              )), widget.divider ?? SizedBox.shrink()],
-                      ))
-                      .toList(),
-                  ],),
-              ),
-            )
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: 200,
+        color: widget.backgroundColor,
+        decoration: widget.decoration,
+        child: Scrollbar(
+          controller: _scrollController,
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [...widget.columns
+                  .asMap()
+                  .entries
+                  .map((e) =>
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(onTap: () => widget.onTap?.call(e.key),
+                          child: MouseRegion(
+                            cursor: SystemMouseCursors.click,
+                            onEnter: (_) {
+                              setState(() {
+                                _highlightIndex = e.key;
+                              });
+                            },
+                            onExit: (_) {
+                              setState(() {
+                                _highlightIndex = -1;
+                              });
+                            },
+                            child: Container(
+                              color: _highlightIndex == e.key ? Colors.grey
+                                  .withOpacity(0.2) : Colors.transparent,
+                              child: Container(
+                                padding: EdgeInsets.symmetric(horizontal: widget.horizontalPadding ?? 0),
+                                child: e.value,
+                              ),
+                            ),
+                          )), widget.divider ?? SizedBox.shrink()],
+                  ))
+                  .toList(),
+              ],),
+          ),
         )
     );
   }
